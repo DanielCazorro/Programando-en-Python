717f0d9c7d2d999e3306795263edf644b0e91e92
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-04 11:19:31 +0100

    ft: añadimos descripción

--- Sueldo01.py
+++ Sueldo01.py
@@ -1,3 +1,4 @@
+# Realizamos un programa básico que calcula la retención y las pagas mensuales en función del sueldo a percibir
 sueldos = (12450, 20200, 35200, 60000, 70000, 100000, 10000000)
 retenciones = (1.55, 13.32, 20.08, 26.84, 29.91, 33.94, 44.88)
 
