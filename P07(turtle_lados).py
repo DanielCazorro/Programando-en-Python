caa37b166e540edbb7209b0d56831de1a9dd13e3
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-04 10:33:05 +0100

    feat: programa de elección de lados

---
+++ P07(turtle_lados).py
@@ -0,0 +1,20 @@
+import turtle
+
+miTortuga = turtle.Turtle()
+
+strLados = input("¿Cuántos lados quieres? ")
+lados = int(strLados)
+
+if lados == 3:
+
+    for _ in range(0, lados):
+        miTortuga.forward(100)  # Aquí avanzamos 100 pixels
+        miTortuga.left(120)  # Aquí giramos 120 grados
+
+elif lados == 4:
+    for _ in range(0, lados):
+        miTortuga.forward(100)  # Aquí avanzamos 100 pixels
+        miTortuga.left(90)  # Aquí giramos 90 grados
+
+else:
+    print("No se dibujar más que cuadrados y triángulos")
