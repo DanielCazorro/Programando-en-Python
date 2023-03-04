81f5a7c51547fc22e46d2e447e1402d6b9949f33
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-04 10:03:56 +0100

    ft: creamos un octógono

---
+++ P03(turtle2).py
@@ -0,0 +1,21 @@
+import turtle
+
+miTortuga = turtle.Turtle()
+
+# Aquí es dibujamos un octógono
+miTortuga.forward(50)
+miTortuga.left(45)
+miTortuga.forward(50)
+miTortuga.left(45)
+miTortuga.forward(50)
+miTortuga.left(45)
+miTortuga.forward(50)
+miTortuga.left(45)
+miTortuga.forward(50)
+miTortuga.left(45)
+miTortuga.forward(50)
+miTortuga.left(45)
+miTortuga.forward(50)
+miTortuga.left(45)
+miTortuga.forward(50)
+miTortuga.left(45)
