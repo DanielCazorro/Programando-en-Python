f460b477bf71f32bbf8086b58ffdeab324990a78
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-04 11:13:32 +0100

    ft: operaciones básicas y agas mensuales

--- Sueldo01.py
+++ Sueldo01.py
@@ -7,3 +7,18 @@ miSueldo = float(strMiSueldo)
 ix = 0
 while miSueldo >= sueldos[ix]:
     ix = ix + 1
+
+retencion = retenciones[ix - 1] + ((miSueldo - sueldos[ix - 1]) * (
+    retenciones[ix] - retenciones[ix - 1]) / (sueldos[ix] - sueldos[ix - 1]))
+
+retencionTotal = retencion + 6.35
+
+miSueldoNeto = miSueldo * (100-retencionTotal)/100
+
+
+mensual12 = miSueldoNeto/12
+mensual14 = miSueldoNeto/14
+
+print(f"Mi sueldo neto: {miSueldoNeto}")
+print(f"12 pagas de: {mensual12}")
+print(f"14 pagas de: {mensual14}")
