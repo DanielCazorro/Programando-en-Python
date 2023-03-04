002faf6064fc0c91e420cc13eccca8eaaf085550
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-04 10:38:23 +0100

    ft: turtle dibuja movimento la figura en bucle for

---
+++ P08(turtle_giro).py
@@ -0,0 +1,14 @@
+import turtle
+
+miTortuga = turtle.Turtle()
+
+strLados = input("¿Cuántos lados quieres? ")
+lados = int(strLados)
+strTam = input("¿De que tamaño los quieres? ")
+Tam = int(strTam)
+
+for _ in range(0, 360, 15):
+    for _ in range(0, lados):
+        miTortuga.forward(Tam)
+        miTortuga.left(360/lados)
+    miTortuga.left(15)
