a3a16aa74ade84e000b7f64b5d0143a0b25b0c95
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-04 11:05:39 +0100

    ft: comenzamos con variables y bucle while

--- Sueldo01.py
+++ Sueldo01.py
@@ -1,2 +1,9 @@
 sueldos = (12450, 20200, 35200, 60000, 70000, 100000, 10000000)
 retenciones = (1.55, 13.32, 20.08, 26.84, 29.91, 33.94, 44.88)
+
+strMiSueldo = input("Introduce sueldo: ")
+miSueldo = float(strMiSueldo)
+
+ix = 0
+while miSueldo >= sueldos[ix]:
+    ix = ix + 1
