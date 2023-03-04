e352b63c6209691a7382610b39ed57628cab1c0c
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-04 10:17:02 +0100

    ft: corregimos texto triángulo

--- P05(triangulo_for).py
+++ P05(triangulo_for).py
@@ -4,4 +4,4 @@ miTortuga = turtle.Turtle()
 
 for _ in range(0, 3):
     miTortuga.forward(50)  # Aquí avanzamos 50 pixels
-    miTortuga.left(120)  # Aquí giramos 90 grados
+    miTortuga.left(120)  # Aquí giramos 120 grados
