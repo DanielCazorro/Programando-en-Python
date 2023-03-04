74acf9b824ef9aee16e922ee7074e060b1b15fa8
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-04 10:22:29 +0100

    ft: programa con elección de turtle con if

---
+++ P06(tr_o_cd).py
@@ -0,0 +1,16 @@
+import turtle
+
+miTortuga = turtle.Turtle()
+
+respuesta = input("¿Quieres un triángulo?(S/N) ")
+
+if respuesta == 'S':
+
+    for _ in range(0, 3):
+        miTortuga.forward(50)  # Aquí avanzamos 50 pixels
+        miTortuga.left(120)  # Aquí giramos 120 grados
+
+else:
+    for _ in range(0, 4):
+        miTortuga.forward(50)  # Aquí avanzamos 50 pixels
+        miTortuga.left(90)  # Aquí giramos 120 grados
