c910f71db93939e8e22b81934f02c4949df8a9eb
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-05 10:54:40 +0100

    ft: añadimos tipo int a los números

--- Programas sencillos/Año_nacimiento.py
+++ Programas sencillos/Año_nacimiento.py
@@ -1,9 +1,11 @@
 nombre = input("¿Cómo te llamas? ")
 print("Hola", nombre)
 
-edad = input("¿Cuántos años tienes? ")
+StrEdad = input("¿Cuántos años tienes? ")
+edad = int(StrEdad)
 
-a_actual = input("¿En qué año estamos? ")
+StrA_actual = input("¿En qué año estamos? ")
+a_actual = int(StrA_actual)
 
 a_nacimiento = a_actual - edad
 
