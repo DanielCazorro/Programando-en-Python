b63723eafea83279b7d62855e94f54b2d6e78796
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-05 20:09:15 +0100

    ft: añadimos bucles if para exención

---
+++ Cálculo IRFP/IRPF.py
@@ -0,0 +1,31 @@
+# Calcular el porcentaje de retención para el IRPF en función de mi sueldo y circunstancias
+
+# Obtención de datos
+sueldo = float(input("Sueldo: "))
+situacion = input("Situación (1/2/3)")
+num_hijos = int(input("Número de hijos: "))
+
+# Calcular exención
+if situacion == '1':
+    if num_hijos <= 0:
+        exencion = 0
+    elif num_hijos == 1:
+        exencion = 17270
+    else:
+        exencion = 18617
+
+if situacion == '2':
+    if num_hijos <= 0:
+        exencion = 16696
+    elif num_hijos == 1:
+        exencion = 17894
+    else:
+        exencion = 19241
+
+if situacion == '1':
+    if num_hijos <= 0:
+        exencion = 15000
+    elif num_hijos == 1:
+        exencion = 15599
+    else:
+        exencion = 16272
