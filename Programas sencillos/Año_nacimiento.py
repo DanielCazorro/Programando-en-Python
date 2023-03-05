35fa293456af942367ad8c2eaebfc1ba9913bc9b
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-05 10:53:04 +0100

    ft: comenzamos a crear variables

---
+++ Programas sencillos/Año_nacimiento.py
@@ -0,0 +1,10 @@
+nombre = input("¿Cómo te llamas? ")
+print("Hola", nombre)
+
+edad = input("¿Cuántos años tienes? ")
+
+a_actual = input("¿En qué año estamos? ")
+
+a_nacimiento = a_actual - edad
+
+print(nombre, "naciste en", a_nacimiento)
