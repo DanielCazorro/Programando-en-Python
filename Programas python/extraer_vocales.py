770d7ea88fb6633c52df4e979d2d18a4427ed02f
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-08 11:15:39 +0100

    ft: función para extraer vocales

---
+++ Programas python/extraer_vocales.py
@@ -0,0 +1,6 @@
+def extraer_vocales(palabra):
+    vocales = ''
+    for caracter in palabra:
+        if caracter.upper() in 'AEIOU':
+            vocales += caracter
+    print(f"Las vocales de su palabra {palabra} son: {vocales}")
