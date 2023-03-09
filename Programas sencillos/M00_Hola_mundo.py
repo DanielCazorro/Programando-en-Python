773ae002aa3c28025bdc9a918d1e498ab6cf6d52
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-09 12:05:17 +0100

    ft: programa sencillo hola mundo

---
+++ Programas sencillos/M00_Hola_mundo.py
@@ -0,0 +1,10 @@
+# Hola, mundo
+# Hacer un programa que pida el nombre y te salude por tu nombre
+
+# Restricciones
+# Mantener entrada, salida y concatenación separados
+nombre = input("Escriba su nombre: ")
+
+texto = f"Hola"
+
+print(texto, nombre)
