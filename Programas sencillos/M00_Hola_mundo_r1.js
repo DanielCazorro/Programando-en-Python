6aa0756839345397ba0deee686d5ca61a318a36e
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-09 12:17:19 +0100

    ft: hola con reto

---
+++ Programas sencillos/M00_Hola_mundo_r1.js
@@ -0,0 +1,10 @@
+// # Hola, mundo
+// # Hacer un programa que pida el nombre y te salude por tu nombre
+
+// # Restricciones
+// # Mantener entrada, salida y concatenación separados
+
+// # Retos
+// # Escribir el programa sin usar variables
+
+alert("Hola, " + prompt("¿Cómo te llamas? ") + ". Encantado de conocerte")
