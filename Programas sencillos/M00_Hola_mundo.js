ebef90d131b9a68fba45675693cd8993c90e0ff7
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-09 12:28:36 +0100

    ft: hola sencillo

---
+++ Programas sencillos/M00_Hola_mundo.js
@@ -0,0 +1,10 @@
+// # Hola, mundo
+// # Hacer un programa que pida el nombre y te salude por tu nombre
+
+// # Restricciones
+// # Mantener entrada, salida y concatenación separados
+nombre = prompt("Escriba su nombre: ")
+
+texto = "Hola "
+
+alert(texto + nombre)
