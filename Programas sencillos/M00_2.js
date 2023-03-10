567ec16fa337e9e240a476689dbc7d90fa57031d
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-10 10:31:21 +0100

    ft: cuenta los caracteres de una cadena

---
+++ Programas sencillos/M00_2.js
@@ -0,0 +1,10 @@
+// # El programa pedirá una cadena de caracteres y devolverá el número de caracteres.
+
+// # 1. Asegurate de que devuelve la cadena original
+// # 2. Utiliza la función específica de python para resolverlo
+
+const cadena = prompt("Introduzca la cadena de caracteres que desea: ");
+const length_cadena = cadena.length;
+
+console.log(`Su cadena tiene un número de caracteres de: ${length_cadena}`);
+alert(`Su cadena tiene un número de caracteres de: ${length_cadena}`);
