4342cdefe0686e4c9e6514b47bd6c4c8598552c7
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-10 10:25:52 +0100

    ft: funcion que devuelva número de cadena

---
+++ Programas sencillos/M00_2.py
@@ -0,0 +1,9 @@
+# El programa pedirá una cadena de caracteres y devolverá el número de caracteres.
+
+# 1. Asegurate de que devuelve la cadena original
+# 2. Utiliza la función específica de python para resolverlo
+
+cadena = input("Introduzca la cadena de caracteres que desea: ")
+length_cadena = len(cadena)
+
+print(f"Su cadnea tiene un número de caracteres de: {length_cadena}")
