e81d1746f21e3df99bf6118742f49389d9666965
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-10 10:05:09 +0100

    ft: añadimos cálculo de media

--- Programas sencillos/nota_num.py
+++ Programas sencillos/nota_num.py
@@ -9,3 +9,20 @@ def nota_numerica(letra):
         puntero += 1
 
     return notas[puntero]
+
+
+num_notas = 0
+sum_notas = 0
+
+nota = input("Nota o vacío para terminar: ")
+
+while nota != "":
+    num_notas += 1
+    valor_nota = nota_numerica(nota)
+    sum_notas += valor_nota
+
+    nota = input("Nota o vacío para terminar: ")
+
+media = sum_notas / num_notas
+
+print(f"La nota media es: {media}")
