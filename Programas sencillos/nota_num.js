a5d91f27b265b3ef3f6f467072661a8742d02b1c
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-10 09:54:49 +0100

    ft: movemos para ordenar

---
+++ Programas sencillos/nota_num.js
@@ -0,0 +1,13 @@
+function notaNumerica(letra) {
+    letras = ['A+', 'A', 'A-', 'B+', 'B',
+        'B-', 'C+', 'C', 'C-', 'D+', 'D', 'F']
+    notas = [4, 4, 3.7, 3.3, 3, 2.7, 2.3, 2, 1.7, 1.3, 1, 0]
+
+    puntero = 0
+
+    while (letras[puntero] != letra) {
+        puntero += 1
+    }
+
+    return notas[puntero]
+}
