b5b7ffed3ee9fce4889de033f527f8bef2b7d55e
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-12 11:35:22 +0100

    ft: añadimos errores

--- Programas sencillos/nota_num.js
+++ Programas sencillos/nota_num.js
@@ -1,6 +1,5 @@
 function notaNumerica(letra) {
-    letras = ['A+', 'A', 'A-', 'B+', 'B',
-        'B-', 'C+', 'C', 'C-', 'D+', 'D', 'F']
+    letras = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'F']
     notas = [4, 4, 3.7, 3.3, 3, 2.7, 2.3, 2, 1.7, 1.3, 1, 0]
 
     puntero = 0
@@ -9,5 +8,34 @@ function notaNumerica(letra) {
         puntero += 1
     }
 
+    if (puntero == letras.lenght) {
+        throw new Error("Nota " + letra + " inexistente.")
+    }
     return notas[puntero]
+}
+
+numNotas = 0
+sumNotas = 0
+
+nota = prompt("Nota o vacío para acabar")
+
+while (nota != "") {
+    nota = nota.toUpperCase()
+
+    try {
+        valorNota = notaNumerica(nota)
+        numNotas += 1
+        sumNotas += valorNota
+    } catch (e) {
+        alert("Nota " + nota + " no es correcta. Vuelve a intentarlo.")
+    }
+
+    nota = prompt("Nota o vacío para acabar")
+}
+
+if (numNotas == 0) {
+    alert("No se han introducido datos")
+} else {
+    media = sumNotas / numNotas
+    alert("La media es: " + media)
 }
