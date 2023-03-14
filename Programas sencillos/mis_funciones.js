937087dcf656b6d04bc6f2d0af96065bcd664780
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-14 18:31:28 +0100

    ft: mis funciones en un archivo js

---
+++ Programas sencillos/mis_funciones.js
@@ -0,0 +1,42 @@
+function nota_numerica(letra) {
+    const letras = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'F'];
+    const notas = [4, 4, 3.7, 3.3, 3, 2.7, 2.3, 2, 1.7, 1.3, 1, 0];
+
+    let puntero = 0;
+
+    while (letras[puntero] !== letra) {
+        puntero++;
+    }
+
+    return notas[puntero];
+}
+
+function pedir_nota_correcta() {
+    let valor = "";
+    while (valor === "") {
+        const nota = prompt("Nota: ").toUpperCase();
+
+        try {
+            valor = nota_numerica(nota);
+        } catch (e) {
+            console.log(`Nota ${nota} incorrecta. Vuelva a introducir.`);
+            valor = "";
+        }
+    }
+
+    return valor;
+}
+
+function pedir_numero() {
+    let valor = "";
+    while (valor === "") {
+        try {
+            valor = parseInt(prompt("Número de asignaturas: "));
+        } catch (e) {
+            console.log("Debe ser un número entero. Vuelva a introducirlo.");
+            valor = "";
+        }
+    }
+
+    return valor;
+}
