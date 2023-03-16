fa5bd0e53b48fe994b05038bced2815cb4a7d404
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-16 09:43:24 +0100

    ft: programa que reparte pizzas y porciones

---
+++ Programas sencillos/M00_8.py
@@ -0,0 +1,47 @@
+# # Repartiendo pizza
+
+# Escribir un programa que sabiendo cuantas personas hay en una reunión y cuantas pizzas se han comprado, queriendo que cada persona tenga una porción de cada pizza.
+# La pizza sólo puede dividirse en un número par de porciones
+
+# ```
+# ¿Número de personas? 7
+# ¿Número de pizzas? 3
+
+# 7 personas con 3 pizzas
+# Cada persona toma 3 piezas
+# Sobran 3 porciones
+# ```
+# Retos
+
+# 1. Asegurar que la entrada es numerica, positiva y entera. En otro caso impedir que el usuario continue
+
+
+# Datos:
+pizza = 0
+while pizza <= 0:
+    try:
+        pizza = input("¿Cuántas pizzas se han comprado?: ")
+        pizza = int(pizza)
+    except:
+        print("Debe introducir un número entero y positivo de pizzas.")
+        pizza = 0
+
+personas = 0
+while personas <= 0:
+    try:
+        personas = input("¿Cuántas personas hay?: ")
+        personas = int(personas)
+    except:
+        print("Debe introducir un número entero y positivo de personas.")
+        personas = 0
+
+# Operaciones:
+if personas % 2 == 1:
+    porciones = personas + 1
+else:
+    porciones = personas
+
+sobrante = (porciones*pizza) % personas
+
+# Resultados:
+print(f"Hay {personas} personas y {pizza} pizzas.\nCada persona toma {pizza} porciones.\nSobran {sobrante} porciones")
