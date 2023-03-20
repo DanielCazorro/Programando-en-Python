ade1478ea4c3f2307b45998500adc55dcb1ddfdc
LeinadHunter <leinadhunter10@hotmail.com>
2023-03-20 19:00:20 +0100

    ft: cambios nombres

--- Cálculo IRFP/f_retenciones1.py
+++ Cálculo IRFP/f_retenciones1.py
@@ -4,10 +4,10 @@
 RETENCIONES = [[0, 0], [12450, 19], [20200, 24], [35200, 30],
                [60000, 37], [300000, 45], [float('inf'), 47]]
 
-SITUACIONES = {'1': [0, 15947, 17100],
-               '2': [15546, 16481, 17634],
-               '3': [14000, 14516, 15063]
-               }
+EXENCIONES = {'1': [0, 15947, 17100],
+              '2': [15546, 16481, 17634],
+              '3': [14000, 14516, 15063]
+              }
 
 # SIT1 = [0, 15947, 17100]
 # SIT2 = [15546, 16481, 17634]
@@ -20,7 +20,7 @@ def obtener_exencion(sit, nhijos):
     elif nhijos < 0:
         nhijos = 0
 
-    return SITUACIONES[sit][nhijos]
+    return EXENCIONES[sit][nhijos]
 
 
 # def obtener_retencion_while(base):
