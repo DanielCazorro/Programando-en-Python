242bb85a3a1f0fa4966e239a4f122c37eb7d2516
LeinadHunter <leinadhunter10@hotmail.com>
2023-04-17 12:49:34 +0200

    ft

--- Programas sencillos/Funciones de orden superior/test_sumas.py
+++ Programas sencillos/Funciones de orden superior/test_sumas.py
@@ -1,3 +1,10 @@
-from sumas import sumaTodos
+from sumas import sumaTodos, sumaTodosAlCuadrado, sumatorio
 
-print(sumaTodos(100))  # 5050
+print(sumaTodos(100), sumatorio(100))  # 5050
+
+
+def cuadrado(x):
+    return x ** 2
+
+
+print(sumaTodosAlCuadrado(3), sumatorio(3, cuadrado))  # 14
