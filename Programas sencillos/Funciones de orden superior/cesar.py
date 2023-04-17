ef7daa48c3061c5d68aa1620a3e4440e062af148
LeinadHunter <leinadhunter10@hotmail.com>
2023-04-17 11:38:27 +0200

    ft: Solucion con el resto

--- Programas sencillos/Funciones de orden superior/cesar.py
+++ Programas sencillos/Funciones de orden superior/cesar.py
@@ -4,4 +4,5 @@ ALFABETO = list('ABCDEFGHIJKLMNÑOPQRSTUVWXYZ')
 def cesar(caracter, clave):
     posicion = ALFABETO.index(caracter)
     nposicion = posicion + clave
+    nposicion = nposicion % len(ALFABETO)
     return ALFABETO[nposicion]
