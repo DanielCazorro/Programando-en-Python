54f36d945dd9f26a880987d6fd6ed120d78b0cad
LeinadHunter <leinadhunter10@hotmail.com>
2023-04-17 12:44:23 +0200

    ft: función de suma y su prueba

---
+++ Programas sencillos/Funciones de orden superior/sumas.py
@@ -0,0 +1,5 @@
+def sumaTodos(n):
+    resultado = 0
+    for i in range(n + 1):
+        resultado += i
+    return resultado
---
+++ Programas sencillos/Funciones de orden superior/test_sumas.py
@@ -0,0 +1,3 @@
+from sumas import sumaTodos
+
+print(sumaTodos(100))  # 5050
