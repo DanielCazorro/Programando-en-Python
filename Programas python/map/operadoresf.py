940da914b3b36a43bcd444ba34c038781ac0a107
LeinadHunter <leinadhunter10@hotmail.com>
2023-04-17 15:46:10 +0200

    ft: Probando filter

--- Programas python/map/operadoresf.py
+++ Programas python/map/operadoresf.py
@@ -6,8 +6,8 @@ def voz_baja(texto):
     return chr(129323) + texto.lower()
 
 
-frutas = ["plátano", "plátano", "fresa",
-          "naranja", "uva", "uva", "fresa", "plátano"]
+frutas = ["platano", "platano", "fresa",
+          "naranja", "uva", "uva", "fresa", "platano", ""]
 
 lfConAsterisco = list(map(lambda p: '*' + p + '*', frutas))
 lMayusculas = list(map(voz_alta, frutas))
@@ -21,3 +21,16 @@ for fruta in frutas:
     lMayusculasIterando.append(voz_alta(fruta))
 
 print(lMayusculasIterando)
+
+
+lp3aletraA = list(filter(lambda p: len(p) > 2 and p[2] == 'a', frutas))
+
+
+def es3A(p):
+    return len(p) > 2 and p[2] == 'a'
+
+
+lp3aletraAF = list(filter(es3A, frutas))
+
+print(lp3aletraA)
+print(lp3aletraAF)
