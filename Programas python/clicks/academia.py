865568cc340f585db9f5d1b8b2a6807c2e54f859
LeinadHunter <leinadhunter10@hotmail.com>
2023-04-18 16:53:56 +0200

    ft: Asignaturas

--- Programas python/clicks/academia.py
+++ Programas python/clicks/academia.py
@@ -1,18 +1,3 @@
-class Alumno():
-
-    def __init__(self, n, ap):
-        self.nombre = n
-        self.apellidos = ap
-        self.movil = ''
-        self.correo_e = ''
-
-    def __str__(self):
-        return 'Alumno: {} {} - {}'.format(self.nombre, self.apellidos, self.correo_e)
-
-    def __repr__(self):
-        return self.__str__()
-
-
 class Asignatura():
     def __init__(self, nombre, nivel):
         self.nombre = nombre
@@ -21,6 +6,43 @@ class Asignatura():
 
     def __str__(self):
         return 'Asignatura: {} - {} ({:.2f} €/mes)'.format(self.nombre, self.nivel, self.precio_h * 4)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Alumno():
+
+    def __init__(self, n, ap):
+        self.nombre = n
+        self.apellidos = ap
+        self.movil = ''
+        self.correo_e = ''
+
+        self.asignatura = None
+
+    def alta_asignatura(self, asignatura):
+        if not isinstance(asignatura, Asignatura):
+            raise Exception(
+                '{} debe ser del tipo Asignatura'.format(asignatura))
+
+        self.asignatura = asignatura
+
+    """
+        self.asignaturas = []
+
+    def alta_asignatura(self, asignatura):
+        if not isinstance(asignatura, Asignatura):
+            raise Exception(
+                '{} debe ser del tipo Asignatura'.format(asignatura))
+
+        if not asignatura in self.asignaturas():
+            self.asignaturas.append(asignatura)
+
+    """
+
+    def __str__(self):
+        return 'Alumno: {} {} - {}'.format(self.nombre, self.apellidos, self.correo_e)
 
     def __repr__(self):
         return self.__str__()
--- Programas python/clicks/test_academia.py
+++ Programas python/clicks/test_academia.py
@@ -11,4 +11,12 @@ print(roberto.correo_e, roberto.movil)
 ingles = Asignatura("Inglés", "A2")
 ingles.precio_h = 7.5
 
-print(ingles)  # Asignatura: Inglés - A2 - (7.50 €/mes)
+print(ingles)  # Asignatura: Inglés - A2 - (30.00 €/mes)
+
+roberto.alta_asignatura(ingles)
+
+print(roberto.asignatura)  # Asignatura: Inglés - A2 - (30.00 €/mes)
+print(susana.asignatura)
+
+roberto.asignatura = None
+print(roberto.asignatura)
