064e68ab4a6b1e45d95af4395273b81ee4f1ec8b
LeinadHunter <leinadhunter10@hotmail.com>
2023-04-18 11:20:20 +0200

    ft: Versión final, con explicaciones

--- Programas python/clicks/contador.py
+++ Programas python/clicks/contador.py
@@ -9,9 +9,17 @@ def creaContador(ini=0):
     return click
 
 
+# Creador de la función con comportamiento y estado -> Clase
 def creaContadorReutilizable(ini=0):
+    '''
+    Variables de estado -> Atributos
+    '''
     clicks = ini
 
+    '''
+    reset, consulta, click
+    Funcioens de comportamiento -> Métodos
+    '''
     def reset(v):
         nonlocal clicks
         clicks = v
@@ -20,12 +28,20 @@ def creaContadorReutilizable(ini=0):
     def consulta():
         return clicks
 
+    def click():
+        nonlocal clicks
+        clicks += 1
+        return clicks
+
+    '''
+    Parte que crea lo necesario para que el contador funcione (infraestructura mínima) -> Constructor
+    incluye el return
+    '''
     def contador(**kwargs):
         nonlocal clicks
 
         if len(kwargs) == 0:
-            clicks += 1
-            return clicks
+            return click()
 
         if 'consulta' in kwargs:
             return consulta()
