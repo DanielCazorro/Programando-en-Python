9efb1ab83a3857416371fc7dae3ddfef12481b1e
LeinadHunter <leinadhunter10@hotmail.com>
2023-05-03 12:40:37 +0200

    ft: calculo IVA javascript

---
+++ Programas sencillos/M00_19.js
@@ -0,0 +1,51 @@
+// # IVA general por paises
+// # Según el pais se aplica un tipo general de IVA u otro. Haz un programa que aplique el tipo de iva adecuado según el pais de origen
+
+// # Seguir la siguiente tabla. El programa debe mostrar la base, el iva aplicado en % y en € y el precio final.
+
+// # Restricciones
+// # Las cantidades en € deben aparecer ajustadas al céntimo
+// # Utilizar una sola instrucción de impresión de resultados
+
+const ivas = {
+    "hungria": 27,
+    "croacia": 25,
+    "dinamarca": 25,
+    "suecia": 25,
+    "finlandia": 24,
+    "rumania": 24,
+    "grecia": 23,
+    "irlanda": 23,
+    "polonia": 23,
+    "portugal": 23,
+    "eslovenia": 22,
+    "italia": 22,
+    "españa": 21,
+    "belgica": 21,
+    "letonia": 21,
+    "lituania": 21,
+    "paises bajos": 21,
+    "republica checa": 21,
+    "austria": 20,
+    "bulgaria": 20,
+    "eslovaquia": 20,
+    "estonia": 20,
+    "francia": 20,
+    "reino unido": 20,
+    "alemania": 19,
+    "chipre": 19,
+    "malta": 18,
+    "luxemburgo": 19
+};
+
+const pais = prompt("Pais de origen: ");
+
+let precio = prompt("Precio: ");
+precio = parseFloat(precio);
+
+const iva = precio * (ivas[pais.toLowerCase()] / 100);
+
+const total = precio + iva;
+
+console.log(`Base: ${precio.toFixed(2)}€\nIVA: ${ivas[pais.toLowerCase()]}% + ${iva.toFixed(2)}€\nTotal: ${total.toFixed(2)}€`);
+
