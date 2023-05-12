c79f8717b6bfdf956bf38a176fc9eb7aba32ab8d
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-05-12 10:55:55 +0200

    ft: Mailing adress JavaScript

---
+++ Exercises/Exercise 1.js
@@ -0,0 +1,13 @@
+// # Exercise 1: Mailing Address
+// # Create a program that displays your name and complete mailing address. The address should be printed in the format that is normally used in the area where you live. Your
+// # program does not need to read any input from the user.
+
+function mailing_address(name, address) {
+    /*
+    Program that displays your name and full address
+    */
+    console.log(`Your name is: ${name}\nYour full address is: ${address}`);
+  }
+  
+  mailing_address('Daniel', 'Málaga 24');
+  
