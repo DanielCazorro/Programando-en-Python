d8a3bd3c08d51ae5a346f50d3beaf5b5bfc11611
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-05-12 10:52:53 +0200

    ft: Mailing Adress

---
+++ Exercises/Exercise 1.py
@@ -0,0 +1,11 @@
+# Exercise 1: Mailing Address
+# Create a program that displays your name and complete mailing address. The address should be printed in the format that is normally used in the area where you live. Your
+# program does not need to read any input from the user.
+
+def mailing_address(name, adress):
+    """
+    Programa que muestra tu nombre y tu dirección completa
+    """
+    print(f"Your name is: {name}\nYour full adress is: {adress}")
+
+mailing_address('Daniel', 'Málaga 24')
