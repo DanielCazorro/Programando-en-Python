bd6648a6009c008e63f226b1bfa2f33d78a63f19
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-05-12 11:22:29 +0200

    ft: Hello JavaScript

---
+++ Exercises/Exercise 2.js
@@ -0,0 +1,14 @@
+// # Exercise 2: Hello
+// # Write a program that asks the user to enter his or her name. The program should
+// # respond with a message that says hello to the user, using his or her name.
+
+function hello() {
+    /*
+    Program that asks for your name and greets you
+    */
+    let name = prompt("Escriba su nombre:");
+    console.log(`Hola ${name}! Es un placer estar aquí con usted.`);
+  }
+  
+  hello();
+  
